3510b28caf12baa6320695c227f0c5fb7124c99c
EDMG <edwind.m115@gmail.com>
2025-04-21 19:43:35 -0600

    Definicion de logicas

--- src/modelo/logica/logicaAdministrador.java
+++ src/modelo/logica/logicaAdministrador.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package controladores;
+package modelo.logica;
 
 /**
  *
  * @author edwin
  */
-public class ControladorUsuario {
+public class logicaAdministrador {
     
 }
---
+++ src/modelo/logica/logicaHuesped.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo.logica;
+
+/**
+ *
+ * @author edwin
+ */
+public class logicaHuesped {
+    public void logicaAgregarHuesped(){
+    
+    }
+    
+    public void logicaModificarHuesped(){
+    
+    }
+
+    public Huesped logicaBuscarHuesped(){
+    
+    }
+            
+    
+}
