ac996f4b5b88ec2cd3f8c30a9215a6782ce1cf7a
EDMG <edwind.m115@gmail.com>
2025-04-23 17:41:22 -0600

    Revert "l"
    This reverts commit 36b09df4d83364ee7ae11c915a973c41aad43644.

--- src/controlador/ControladorSelectLogin.java
+++ src/controlador/ControladorSelectLogin.java
@@ -1,12 +1,5 @@
 package controlador;
 
-import vista.ventanas.Login;
-import vista.ventanas.SelectLogin;
-
 public class ControladorSelectLogin {
-    private Login login;
-    public void selectLogin(int seleccion){
-        login = new Login(seleccion);
-        login.setVisible(true);
-    }
+    
 }
--- src/modelo/dao/daoLogin.java
+++
@@ -1,8 +0,0 @@
-package modelo.dao;
-
-import repositorio.RepAdministradores;
-
-public class daoLogin {
-    RepAdministradores ra = new RepAdministradores();
-    
-}
