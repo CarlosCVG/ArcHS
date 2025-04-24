c17a0ecc62ca64e765f3ccd8fba675c8162b2993
CarlosCVG <L23550322@chihuahua2.tecnm.mx>
2025-04-23 19:07:58 -0600

    main

--- src/vista/ArcHotelSystem.java
+++ src/vista/ArcHotelSystem.java
@@ -8,6 +8,7 @@ public class ArcHotelSystem {
 
     public static void main(String[] args) {
         SelectLogin selectlogin = new SelectLogin();
+        selectlogin.setBounds(300, 50, selectlogin.getWidth(), selectlogin.getHeight());
         selectlogin.setVisible(true);
     }
 }
